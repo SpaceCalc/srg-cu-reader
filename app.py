f58677baed3eba70d73ed556b379fe33803efece
Maksim Philippov <vetrume@gmail.com>
2022-10-27 14:29:00 +0300

    добавить иконку приложения

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from MainWindow import MainWindow
+import resources
 
 def main() -> None:
     app = QApplication(sys.argv)
@@ -10,6 +11,7 @@ def main() -> None:
     app.setOrganizationName('NPOL517')
     app.setApplicationName('srg-cu-reader')
     app.setApplicationVersion('v1.0')
+    app.setWindowIcon(QIcon(":/icons/icon.ico"))
 
     window = MainWindow(sys.argv[1:])
     window.show()
